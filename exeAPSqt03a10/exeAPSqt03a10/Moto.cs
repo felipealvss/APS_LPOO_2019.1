8da2cdd7f1464359df18d53f6cc54f472df2b19c
Felipe Alves <40322457+felipealvss@users.noreply.github.com>
2019-06-03 17:44:59 -0300

    Add files via upload

---
+++ exeAPSqt03a10/exeAPSqt03a10/Moto.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exeAPSqt10
+{
+    class Moto
+    {
+        private string marca;
+        private string modelo;
+        private string cor;
+        private int marcha;
+        private const int menorMarcha = 0; // Menor marcha
+        private const int maiorMarcha = 5; // Maior marcha
+        private Boolean ligada;
+
+        // Método Construct
+        public Moto(string marca, string modelo, string cor, int marcha, Boolean ligada)
+        {
+            this.marca = marca;
+            this.modelo = modelo;
+            this.cor = cor;
+            this.marcha = marcha;
+            this.ligada = ligada;
+        }
+
+        // Métodos próprios da Classe
+        public void marchaAcima() // Acima
+        {
+            if (marcha < maiorMarcha)
+            {
+                setMarcha(marcha + 1);
+            }
+        }
+        public void marchaAbaixo() // Abaixo
+        {
+            if (marcha > menorMarcha)
+            {
+                setMarcha(marcha - 1);
+            }
+        }
+        public void ligarMoto() // Ligar moto
+        {
+            this.ligada = true;
+        }
+        public void desligarMoto() // Desligar moto
+        {
+            this.ligada = false;
+        }
+
+        // Métodos Set
+        public void setMarca(string marca)
+        {
+            this.marca = marca;
+        }
+        public void setModelo(string modelo)
+        {
+            this.modelo = modelo;
+        }
+        public void setcor(string cor)
+        {
+            this.cor = cor;
+        }
+        public void setMarcha(int marcha)
+        {
+            this.marcha = marcha;
+        }
+        public void setLigado(Boolean ligada)
+        {
+            this.ligada = ligada;
+        }
+
+        // Métodos 'imprimir', substitutos do método Get
+        public string imprimirMarca()
+        {
+            return this.marca;
+        }
+        public string imprimirModelo()
+        {
+            return this.modelo;
+        }
+        public string imprimirCor()
+        {
+            return this.cor;
+        }
+        public int imprimirMarcha()
+        {
+            return this.marcha;
+
+        }
+        public bool imprimirLigada()
+        {
+            return this.ligada;
+        }
+
+    }
+}
---
+++ exeAPSqt03a10/exeAPSqt03a10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exeAPSqt10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Moto m1 = null;
+            m1 = new Moto("Shineray", "Motocicleta", "Vermelha", 1, true);
+
+            Console.WriteLine();
+            Console.WriteLine("Descrições da moto:");
+            Console.WriteLine("= Marca: " + m1.imprimirMarca());
+            Console.WriteLine("= Modelo: " + m1.imprimirModelo());
+            Console.WriteLine("= Cor: " + m1.imprimirCor());
+            Console.WriteLine("= Marcha: " + m1.imprimirMarcha());
+            Console.WriteLine("= Ligada = " + m1.imprimirLigada());
+            Console.WriteLine();
+
+            m1.marchaAcima(); // Aumento de marcha! x1
+            m1.marchaAcima(); // Aumento de marcha! x2
+            Console.WriteLine("Aumento de marcha!");
+            Console.WriteLine("Marcha atual: " + m1.imprimirMarcha());
+            Console.WriteLine();
+
+            m1.marchaAbaixo(); // Redução de marcha! x1
+            Console.WriteLine("Redução de marcha!");
+            Console.WriteLine("Marcha atual: " + m1.imprimirMarcha());
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
