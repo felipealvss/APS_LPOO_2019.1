a8f9182271538d26b23fcd5345df272b5cea5d25
Felipe Alves <40322457+felipealvss@users.noreply.github.com>
2019-06-03 17:05:31 -0300

    Add files via upload

---
+++ exeAPSqt01e02/exeAPSqt01e02/Aluno.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exeAPSqt02
+{
+    class Aluno
+    {
+        private string nomeCompleto;
+        private string matricula;
+        private long cpf;
+
+        // Construtor #1
+        public Aluno(string nomeCompleto, string matricula)
+        {
+            this.nomeCompleto = nomeCompleto;
+            this.matricula = matricula;
+        }
+
+        // Construtor #2
+        public Aluno(string nomeCompleto, long cpf)
+        {
+            this.nomeCompleto = nomeCompleto;
+            this.cpf = cpf;
+        }
+
+        // Métodos Set, recebem valor para atribuição
+        public void setNomeCompleto(string nomeCompleto)
+        {
+            this.nomeCompleto = nomeCompleto;
+        }
+        public void setMatricula(string matricula)
+        {
+            this.matricula = matricula;
+        }
+        public void setCpf(long cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        // Métodos 'imprimir', substitutos do Get
+        public string imprimirNomeCompleto()
+        {
+            return this.nomeCompleto;
+        }
+        public string imprimirMatricula()
+        {
+            return this.matricula;
+        }
+        public long imprimirCpf()
+        {
+            return this.cpf;
+        }
+    }
+}
---
+++ exeAPSqt01e02/exeAPSqt01e02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exeAPSqt02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* QUESTÃO 01 */
+
+            Aluno a1 = new Aluno("Felipe Alves da Silva", "12018110983"); // Instanciamento recebendo Nome completo e No. de matríula
+                a1.setCpf(05475398356);
+            Aluno a2 = new Aluno("Antônio Numes de Limeira", 48948705334); // Instanciamento recebendo Nome completo e CPF
+            a2.setMatricula("12018115478");
+
+            // Métodos 'Imprimir'
+            Console.WriteLine("Dados do 1o aluno: ");
+            Console.WriteLine();
+            Console.WriteLine("Nome: " + a1.imprimirNomeCompleto());
+            Console.WriteLine("No. de Matrícula: " + a1.imprimirMatricula());
+            Console.WriteLine("No. CPF: " + a1.imprimirCpf());
+            Console.WriteLine();
+            Console.WriteLine("================================");
+            Console.WriteLine();
+            Console.WriteLine("Dados do 2o aluno: ");
+            Console.WriteLine();
+            Console.WriteLine("Nome: " + a2.imprimirNomeCompleto());
+            Console.WriteLine("No. de Matrícula: " + a2.imprimirMatricula());
+            Console.WriteLine("No. CPF: " + a2.imprimirCpf());
+            Console.ReadKey();
+        }
+    }
+}
